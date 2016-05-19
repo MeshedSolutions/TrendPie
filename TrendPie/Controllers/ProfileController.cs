4ba1552748eb63147b3c70098107d7e3548bcb7c
Mark Brown <marksbrown@gmail.com>
2016-05-19 15:59:57 -0400

    Add profile and social pages

---
+++ TrendPie/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using TrendPie.Models;
+using TrendPie.Repositories;
+
+namespace TrendPie.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Wallet()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Social()
+        {
+            List<SocialMediaAccount> viewModel = SocialMediaAccountRepository.GetAll();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ TrendPie/Repositories/SocialMediaAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TrendPie.Models;
+
+namespace TrendPie.Repositories
+{
+    public class SocialMediaAccountRepository
+    {
+        public static List<SocialMediaAccount> GetAll()
+        {
+            using (var db = new TrendPie_Entities())
+            {
+                return db.SocialMediaAccounts.ToList();
+            }
+        }
+    }
+}
